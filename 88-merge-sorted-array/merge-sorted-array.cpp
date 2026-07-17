a51c90de0d3fcabf8fe4e06ef18d292822088bdf
Adarsh Pal <adarshpalpal031@gmail.com>
2026-07-17 17:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (12.94%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int p1 = m - 1;
-        int p2 = n - 1;
-        int p = m + n - 1;
-        
-        // Continue while there are still elements to process in nums2
-        while (p2 >= 0) {
-            // If nums1 still has elements and its current element is strictly greater
-            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1];
-                p1--;
-            } else {
-                // Otherwise, nums2's element is greater or equal
-                nums1[p] = nums2[p2];
-                p2--;
+         
+         int i = m-1;
+         int e = m+n-1;
+         int j = n-1;
+
+         while (i>=0 &&  j >=0){
+            if ( nums1[i]>= nums2[j]){
+               nums1[e]=nums1[i];
+               i--;
+               e--;
+            }else {
+               
+               nums1[e]=nums2[j];
+
+               e--;
+               j--;
             }
-            p--; // Move the placement pointer backwards
-        }
+         } while (j>=0){
+            nums1[e]=nums2[j];
+            e--;
+            j--;
+         }
+          
     }
 };
