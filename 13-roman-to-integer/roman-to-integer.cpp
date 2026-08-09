6c90afe0ff01d9b058786fc9cc61ecac448b0074
Adarsh Pal <adarshpalpal031@gmail.com>
2026-08-09 22:56:11 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (94.77%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -7,8 +7,8 @@ public:
         if (c == 'L') return 50;
         if (c == 'C') return 100;
         if (c == 'D') return 500;
-        return 1000; // M
-    }
+        return 1000; // just for the subh 
+    }   // i want to compplte this day of lc so i w=am just adding the comments because in am not in town and i ama too busy today so this is just a fake submission idk whhy ;
 
     int romanToInt(string s) {
         int ans = 0;
