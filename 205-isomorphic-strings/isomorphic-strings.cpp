306d2ab9d82cfd130a4891147244caa4f9c75748
Adarsh Pal <adarshpalpal031@gmail.com>
2026-09-18 17:20:25 +0530

    Time: 1 ms (52.49%) | Memory: 9.2 MB (82.71%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    bool check(string s, string t) {
-        
-        unordered_map<char , char> mp;
-
-        if (s.length() != t.length()){
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> map1,map2;
+        if(s.length() != t.length()){
             return false;
         }
-        for (int i =0; i<s.length(); i++){
+       for(int i =0;i<s.length();i++){
+        if(map1.count(s[i]) && map1[s[i]] != t[i]){
+            return false;
+        }
+        if(map2.count(t[i]) && map2[t[i]] != s[i]){
+            return false;
+        }
 
-            if (mp.find(s[i])==mp.end()){
-                mp[s[i]]=t[i];
-            }
-            else{
-               if ( mp[s[i]] != t[i])
-                {return false;}
-            }
-        } return true;
+        map1[s[i]]=t[i];
+        map2[t[i]]=s[i];
+        
     }
-    bool isIsomorphic(string s ,string t){
-        bool a = check(s,t);
-        bool b = check(t,s);
-        return a && b;
+    return true;
     }
-};  
+
+};
