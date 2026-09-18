051ccc6f7a80ea672264b384bc8af712828ce87d
Adarsh Pal <adarshpalpal031@gmail.com>
2026-09-18 18:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.21%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mp1,mp2;
+         
+         if (s.size() != t.size()) return false;
+
+        for (int i=0; i<s.length(); i++){
+          mp1[s[i]]++;
+          mp2[t[i]]++;
+        }
+        if (mp1==mp2){
+            return true;
+        }
+        return false;
+    }
+};
